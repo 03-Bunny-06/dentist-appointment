3aff3d47ab3e9485398a739a7e628c89e6c2a321
03-Bunny-06 <saikowshik0306@gmail.com>
2026-03-18 11:56:28 +0530

    admin-middleware

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,11 +1,13 @@
 const {Router} = require("express");
-const {adminSignUpController, adminSignInController} = require("../controllers/adminController");
+const { adminSignUpController, adminSignInController } = require("../controllers/adminController");
 const adminMiddleware = require("../middlewares/adminAuthMiddleware");
-const {getAllDentistsController} = require("../controllers/dentistController");
+const { getAllDentistsController } = require("../controllers/dentistController");
+const { getAllAppointmentsController } = require("../controllers/appointmentController");
 const router = Router();
 
 router.post('/signup', adminSignUpController);
 router.post('/signin', adminSignInController);
 router.get('/dentists', adminMiddleware, getAllDentistsController);
+router.get('/appointments', adminMiddleware, getAllAppointmentsController);
 
 module.exports = router;
